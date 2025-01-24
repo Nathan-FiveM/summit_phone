7b48306d00cb4feb5323cc6e7272b01939a9ebfe
Jarvis095 <179588927+Jarvis095@users.noreply.github.com>
2025-01-24 18:06:54 +0530

    update

--- game/server/apps/Settings/events.ts
+++ game/server/apps/Settings/events.ts
@@ -1,6 +1,10 @@
 import { MongoDB } from "@server/sv_main";
 import { generateUUid } from "@shared/utils";
 
+/* RegisterCommand('addSettings', async (source: number, args: string[]) => {
+    
+}, false); */
+
 const generatePhoneNumber = async (): Promise<string> => {
     const number = Math.floor(1000000000 + Math.random() * 9000000000).toString();
     const exists = await MongoDB.findOne('phone_numbers', { number: number });
@@ -8,8 +12,14 @@ const generatePhoneNumber = async (): Promise<string> => {
     return number;
 };
 
-emit('phoneSetting:server:registerClientSettings', async (source: number) => {
-    const citizenId = global.exports['qb-core'].GetPlayerCitizenIdBySource(source);
+async function GeneratePlayerPhoneNumber(citizenId: string) {
+    const number = await generatePhoneNumber();
+    await MongoDB.insertOne('phone_numbers', {
+        _id: generateUUid(),
+        owner: citizenId,
+        number: number,
+    });
+
     await MongoDB.insertOne('phone_settings', {
         _id: citizenId,
         background: {
@@ -28,9 +38,7 @@ emit('phoneSetting:server:registerClientSettings', async (source: number) => {
         useFaceId: false,
         faceIdIdentifier: '',
     });
-    await MongoDB.insertOne('phone_number', {
-        _id: generateUUid(),
-        owner: citizenId,
-        number: await generatePhoneNumber(),
-    });
-});
+
+    return number;
+}
+exports('GeneratePlayerPhoneNumber', GeneratePlayerPhoneNumber);
--- game/server/sv_exports.ts
+++ game/server/sv_exports.ts
@@ -2,6 +2,7 @@ import { Utils } from "./classes/Utils";
 
 async function GetCurrentPhoneNumber(source: number | string) {
     const citizenId = global.exports['qb-core'].GetPlayerCitizenIdBySource(source);
+    if (!citizenId) return false;
     const number = await Utils.GetPhoneNumberByCitizenId(citizenId);
     return number;
 }
