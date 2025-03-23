d2fcfbea519275f581869ff3666d138730de6bf6
Jarvis095 <179588927+Jarvis095@users.noreply.github.com>
2025-03-23 07:27:02 +0530

    Fixes

--- web/src/routers/apps/Pigeon/CreateNewReply.tsx
+++ web/src/routers/apps/Pigeon/CreateNewReply.tsx
@@ -13,8 +13,12 @@ export default function CreateNewReply(props: { show: boolean, tweetId: string, 
         password: '',
         displayName: '',
         avatar: '',
+        bio: '',
+        followers: [],
+        following: [],
         verified: false,
-        notificationsEnabled: false
+        notificationsEnabled: false,
+        createdAt: '',
     });
     const [content, setContent] = useState('');
     const [inputTitle, setInputTitle] = useState('');
