83e701006fa950674495b7c02a08107db98b2cfd
Vector <96924893+vector4721@users.noreply.github.com>
2025-04-03 15:57:50 +0530

    BluePage Logs

--- game/server/apps/BluePage/callback.ts
+++ game/server/apps/BluePage/callback.ts
@@ -1,5 +1,5 @@
 import { onClientCallback, triggerClientCallback } from "@overextended/ox_lib/server";
-import { MongoDB } from "@server/sv_main";
+import { Logger, MongoDB } from "@server/sv_main";
 import { generateUUid } from "@shared/utils";
 
 onClientCallback('bluepage:createPost', async (source, data: string) => {
@@ -16,6 +16,12 @@ onClientCallback('bluepage:createPost', async (source, data: string) => {
     };
     const res = await MongoDB.insertOne('phone_bluepages', dataX);
     await triggerClientCallback('bluepage:refreshPosts', -1, JSON.stringify(dataX));
+    Logger.AddLog({
+        type: 'phone_bluepages',
+        title: 'Post Created',
+        message: `Post '${title}' (ID: ${dataX._id}) created by ${phoneNumber || email}, content: ${content}`,
+        showIdentifiers: false
+    });
 });
 
 onClientCallback('bluepage:getPosts', async (source) => {
@@ -26,6 +32,13 @@ onClientCallback('bluepage:getPosts', async (source) => {
 });
 
 onClientCallback('bluepage:deletePost', async (source, data: string) => {
+    const post = await MongoDB.findOne('phone_bluepages', { _id: data });
     const res = await MongoDB.deleteOne('phone_bluepages', { _id: data });
     await triggerClientCallback('bluepage:refreshDeletePost', -1, data);
+    Logger.AddLog({
+        type: 'phone_bluepages',
+        title: 'Post Deleted',
+        message: `Post '${post.title}' (ID: ${data}) deleted by ${post.phoneNumber || post.email}, content: ${post.content}`,
+        showIdentifiers: false
+    });
 });
