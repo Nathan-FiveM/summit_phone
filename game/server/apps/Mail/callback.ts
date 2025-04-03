b491807b3e4b824d1ca0dc2f729e4275e37347e6
Vector <96924893+vector4721@users.noreply.github.com>
2025-04-03 15:58:40 +0530

    Mail Logs

--- game/server/apps/Mail/callback.ts
+++ game/server/apps/Mail/callback.ts
@@ -1,5 +1,6 @@
 import { onClientCallback } from "@overextended/ox_lib/server";
 import { MailClass } from "./class";
+import { Logger } from "@server/sv_main";
 
 onClientCallback('summit_phone:getEmailMessages', async (source: number, email: string, password: string) => {
     const data = await MailClass.getMailMessages(email, password)
@@ -8,6 +9,13 @@ onClientCallback('summit_phone:getEmailMessages', async (source: number, email: 
 
 onClientCallback('summit_phone:sendEmail', async (source: number, email: string, to: string, subject: string, message: string, images: string[]) => {
     const res = await MailClass.sendMail(email, to, subject, message, images, source);
+    const citizenId = await exports['qb-core'].GetPlayerCitizenIdBySource(source);
+    Logger.AddLog({
+        type: 'phone_mail',
+        title: 'Email Sent',
+        message: `Player ${citizenId} sent an email from ${email} to ${to} with subject "${subject}", content: "${message}"`,
+        showIdentifiers: false
+    });
     return res;
 });
 
@@ -27,5 +35,12 @@ onClientCallback('summit_phone:updateProfileSettings', async (source: number, da
     const parsedData = JSON.parse(data);
     const { email, password, username, avatar } = parsedData;
     const res = await MailClass.updateProfileSettings(email, password, username, avatar);
+    const citizenId = await exports['qb-core'].GetPlayerCitizenIdBySource(source);
+    Logger.AddLog({
+        type: 'phone_mail',
+        title: 'Profile Updated',
+        message: `Player ${citizenId} updated profile for email ${email}.`,
+        showIdentifiers: false
+    });
     return res;
 });
